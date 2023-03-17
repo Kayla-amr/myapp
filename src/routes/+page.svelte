53778a0900f0189c7d697695302556200d78d247
Kayla Macias <kaylamacias.rivera@gmail.com>
2023-03-17 10:32:08 -0600

    Basic Svelte App

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,79 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let title = "Kayla's ToDo App";
+    let tasks = [
+        { id: 1, name: 'My journey with Svelte', status: true },
+        { id: 2, name: 'Blogging with Svelte', status: false },
+        { id: 3, name: 'Why Svelte is so fun', status: true }
+    ];
+
+
+    let newTask = {
+        id: 0,
+        name: '',
+        status: false
+    };
+
+    let addTask = () => {
+        tasks = tasks.concat({
+            id: tasks.length + 1,
+            name: newTask.name,
+            status: newTask.status
+        })
+    }
+
+    let deleteTask = (id) => {
+        tasks = tasks.filter(task => task.id !== id)
+        tasks.splice(tasks.length - 0)
+        tasks = tasks
+        console.log(tasks)
+    }
+
+    
+
+
+</script>
+<div class="main">
+<h1>{title}</h1>
+<div>
+    <input type="text" bind:value={newTask.name} placeholder="Enter new task...">
+    <button on:click={addTask}>Add Task</button>
+    
+    <table>
+        <thead>
+            <tr>
+                <th>Task</th>
+                <th>Status</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each tasks as task}
+            <tr>
+                <td class:checked={task.status} >{task.name}</td>
+                <td>
+                    <input type="checkbox" bind:checked={task.status}>
+                </td>
+                <td>
+                    <button on:click={() => deleteTask(task.id)}>-</button>
+                </td>
+            </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
+</div>
+
+<style> 
+.main{
+    padding: 2%;
+}
+table{
+    padding-top: 20px;
+}
+tr, td{
+    padding: 20px
+}
+	.checked {
+        text-decoration: line-through;
+    }
+</style> 
